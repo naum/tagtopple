7e9196257eb8596894338d255b98138ce7d41525
George Naum Trifanoff <aznaum@gmail.com>
2018-07-08 19:50:00 -0700

    tumblr console dump display fiddling

--- script/app.js
+++ script/app.js
@@ -1,13 +1,43 @@
+const TUMBLR_API_URL = 'http://azspot.net/api/read/json';
+
 class Wrapper extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { image: null }
+        this.state = { tumbdata: {} }
+    }
+    componentDidMount() {
+        this.getTumblrData()
+    }
+    getTumblrData() {
+        return fetch(TUMBLR_API_URL)
+            .then(rsp => rsp.text())
+            .then(data => {
+                const tdhash = JSON.parse(data.slice(22, -2));
+                console.log(tdhash);
+                console.log(tdhash.tumblelog.title);
+                this.setState({
+                    tumbdata: tdhash
+                })
+            })
     }
     render() {
+        let title = "Not set yet.";
+        let posts = [];
+        if (this.state.tumbdata && this.state.tumbdata.tumblelog) {
+            title = this.state.tumbdata.tumblelog.title;
+            posts = this.state.tumbdata.posts;
+        }
         return React.createElement(
             'div',
             { className: 'wrapper' },
             React.createElement(Header),
+            React.createElement(
+                TumblrConsole, 
+                { 
+                    title: title, 
+                    posts: posts, 
+                }
+            ),
             React.createElement(Footer)
         )
     }
@@ -26,6 +56,29 @@ function Header(props) {
         'h1',
         { className: 'heading' },
         'Welcome to Tag Topple!'
+    )
+}
+
+function TumblrConsole({ title, posts }) {
+    return React.createElement(
+        'div',
+        { className: 'tumblrconsole' },
+        React.createElement('h2', {}, title),
+        React.createElement(TumblrPostList, { posts: posts })
+    )
+}
+
+function TumblrPostList({ posts }) {
+    return posts.map(p => 
+        React.createElement(TumblrPostListItem, { key: p.id, post: p })
+    )    
+}
+
+function TumblrPostListItem({ post }) {
+    return React.createElement(
+        'li',
+        { className: 'postdumpline' },
+        post['url-with-slug'] 
     )
 }
 
